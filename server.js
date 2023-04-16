3e6b8f42ea881bf7b72a330d7ba83813b571ae47
koreyawashington <122809994+koreyawashington@users.noreply.github.com>
2023-04-16 12:15:34 -0400

    doneDone

--- server.js
+++ server.js
@@ -24,27 +24,3 @@ app.get('/:number_of_bottles',(req, res)=>{
     res.send(`<h1>This many bottles of beer on the wall:${number_of_bottles}</h1><a href='/${number_of_bottles-1}'>take one down, pass it around</a>`)
 })
 
-
-
-// app.get('/', (req, res)=> {
-//     res.send(`<h1>99 Bottles of beer on the wall</h1> <a href='/98'>take one down, pass it around</a>`)
-   
-// })
-
-// app.get('/:number_of_bottles',(req, res)=>{
-//     console.log(req.params)
-//     const {number_of_bottles}= req.params
-
-//     if (number_of_bottles === "0") {
-//         res.send("<a href='/98'>No more beer, start over!</a>");
-//       } else if (number_of_bottles < 0 || number_of_bottles > 99) {
-//         res.send("<a href='/98'>Not a valid number</a>");
-//       }
-
-//     res.send(`<h1>Number of bottles left:${number_of_bottles}</h1><a href='/${number_of_bottles-1}'>take one down, pass it around</a>`)
-// })
-
-// // Tell the app to listen on port 3000
-// app.listen(3000, function() {
-//     console.log('Listening on port: ${PORT}');
-//    });
